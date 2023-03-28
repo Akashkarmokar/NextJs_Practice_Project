d5569c9ff0db1771a20381120d50e87350131b37
Akash <akashcsemu@gmail.com>
2023-03-28 21:39:59 +0600

    check build 1

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import Typed from 'react-typed'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] })  
 
 export default function Home() {
   return (
